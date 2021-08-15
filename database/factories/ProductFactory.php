1801b0c09544182a0d03ad232897cece7a4765e1
Italo Araujo <italofaraujo@gmail.com>
2021-08-15 18:04:54 -0300

    Create migrations and seeders

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->unique()->sentence(3),
+            'description' => $this->faker->unique()->sentence(15),
+            'category_id' => \App\Models\Category::inRandomOrder()->first()->id
+        ];
+    }
+}
